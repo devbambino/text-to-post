5475ac5d2927eb66adcc4daa9e5c27faf10256ad
Dev Bambino <devbambinoacc@gmail.com>
2024-04-18 16:17:59 +0000

    better ui 2

--- text2post-app/app/api/chat/route.ts
+++ text2post-app/app/api/chat/route.ts
@@ -12,13 +12,11 @@ export async function POST(req: Request) {
 
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
-    //stream: true,
     messages: [
       {
         role: "system",
         content:
-        //`You are a professional blogger and influencer that writes super engaging posts for social networks about different topics. The user is going to give you a large text that you must summarize and re-write to make it suitable for a social network post. The summarized text should be less than 100 words, and should include in the end a list with the top 7 most relevant words from the text as social networks hashtags. The writing style should be charming, engaging but professional. You should also use the summary to create three Twitter posts(less than 280 characters) from it. Return the response as a JSON object that contains the summary fields as the summary generated, and an array called twitter that contains the twitter posts generated.`,
-          `You are a professional blogger and influencer that writes super engaging posts for social networks about different topics. The user is going to give you a large text that you must summarize and re-write to make it suitable for a social network post. The summarized text MUST have less than 250 words, and you have to also include up to 7 relevant hashtags in the end of the summary. The writing style should be charming, engaging but professional. DON'T ADD ANY EXTRA TEXT BESIDES THE SUMMARY AND THE HASHTAGS.`,
+          `You are a professional blogger and influencer that writes super engaging posts for social networks about different topics. The user is going to give you a text that you must summarize it if the text is really large or re-write it if the text is short, to make it suitable for a social network post. The summarized text MUST have less than 250 words, and you have to also include up to 7 relevant hashtags in the end of the summary. The writing style should be charming, engaging but professional. DON'T ADD ANY EXTRA TEXT BESIDES THE SUMMARY AND THE HASHTAGS. DON'T ADD ADDITIONAL NOTES OR RECOMMENDATIONS ON HOW TO USE THE SUMMARY.`,
       },
       {role: "user",
       content: message.content},
@@ -28,7 +26,4 @@ export async function POST(req: Request) {
   console.log("api chat:", response.choices[0].message.content);
 
   return new Response(JSON.stringify(response.choices[0].message.content))
-
-  //const stream = OpenAIStream(response);
-  //return new StreamingTextResponse(stream);
 }
--- text2post-app/app/page.tsx
+++ text2post-app/app/page.tsx
@@ -2,23 +2,16 @@
 
 import { useState } from "react";
 import { Message, useChat } from "ai/react";
+import Image from 'next/image';
 
 export default function Chat() {
-  const { messages} = useChat();
+  const { messages } = useChat();
   const [isLoading, setIsLoading] = useState(false);
 
   const [state, setState] = useState({
     article: ""
   });
 
-  const handleChange = ({
-    target: { name, value },
-  }: React.ChangeEvent<HTMLInputElement>) => {
-    setState({
-      ...state,
-      [name]: value,
-    });
-  };
   const handleInput = ({
     target: { name, value },
   }: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -35,11 +28,8 @@ export default function Chat() {
           <div className="animate-pulse flex flex-col justify-center items-center ">
             <div className="rounded-full bg-slate-700 h-10 w-10"></div>
             <div>Loading...</div>
-
           </div>
-
         </div>
-
       </div>
     );
   }
@@ -53,31 +43,28 @@ export default function Chat() {
       <div className="flex flex-col gap-4 max-w-3xl mx-auto">
         <div className="flex flex-col gap-2">
           <div className="space-y-2">
-            <h2 className="w-full text-center text-3xl font-bold">influencerGPT App</h2>
+            <h2 className="w-full text-center text-3xl text-green-500 font-bold">influencerGPT App</h2>
             <p className="w-full text-center text-zinc-500 dark:text-zinc-400">
-              We create social networks posts for you from articles you shared with us.
+              We create social networks posts for you from whatever you shared with us.
             </p>
           </div>
 
-          <div className="space-y-2flex flex-col gap-2">
+          <div className="flex flex-col gap-2">
             {messages.length == 0 && (
-              <div className="w-full space-y-2">
-                <p className="text-sm leading-6 b-0">
-                  Enter a large block of text here and we'll generate for you a summarized version ready to be posted on social networks:
+              <div className="w-full">
+                <p className="my-2 text-md leading-6 b-0">
+                  Write/Paste here what you want to share and we'll generate for you an engaging version of it ready to be posted on social networks:
                 </p>
-                <textarea className="w-full min-h-[200px] border rounded shadow-xl text-black" name="article" disabled={isLoading} id="text" placeholder="Enter the text here..." onChange={handleInput} />
-
+                <textarea className="w-full min-h-[200px] m-2 border rounded shadow-xl text-black" name="article" disabled={isLoading} id="text" placeholder="Enter the text here..." onChange={handleInput} />
               </div>
             )}
-            <form className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-4">
+            <form className="flex flex-row items-start gap-2 md:gap-4">
               <button
-                className="w-full md:w-auto order-2 md:order-1 m-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
-
+                className="w-full md:w-auto order-2 md:order-1 m-2 bg-green-700 hover:bg-gray-700 text-white py-2 px-4 rounded disabled:opacity-50"
                 hidden={messages.length > 0}
                 disabled={isLoading}
                 onClick={async () => {
                   setIsLoading(true);
-                  console.log("chat article:", state.article);
                   const response = await fetch("api/chat", {
                     method: "POST",
                     headers: {
@@ -88,58 +75,47 @@ export default function Chat() {
                     }),
                   });
                   const data = await response.json();
-                  console.log("chat response:", data);
                   setIsLoading(false);
                   const chats: Message = {
                     "id": "chats",
                     "role": "user",
                     "content": data
-
                   };
                   messages.push(chats);
-
-
                 }}
-              >Generate summary</button>
-
-              {messages.length == 1 && !isLoading && (
-                <button
-                  className="w-full md:w-auto order-1 md:order-2 m-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50" disabled={isLoading}
-                  onClick={async () => {
-                    setIsLoading(true);
-                    const response = await fetch("api/tweets", {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({
-                        message: { role: "user", content: messages[0].content },
-                      }),
-                    });
-                    const data = await response.json();
-                    console.log("tweets response:", data);
-
-                    const cleanedJsonString = data.replace(/^```json\s*|```\s*$/g, '');
-                    const tweetsJson = JSON.parse(cleanedJsonString);
-                    // Loop through each tweet and parse relevant information
-                    tweetsJson.forEach((tweet: { tweet: string; }, index: number) => {
-                      const tweets: Message = {
-                        "id": "tweets",
-                        "role": "assistant",
-                        "content": tweet.tweet
-
-                      };
-                      messages.push(tweets);
-                    });
-
-                    setIsLoading(false);
-
-                  }}>
-                  Generate tweets
-                </button>
-              )}
+              >Generate post</button>
               <button
-                className="w-full md:w-auto order-2 md:order-1 m-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+                className="w-full md:w-auto order-1 md:order-2 m-2 bg-green-700 hover:bg-gray-700 text-white py-2 px-4 rounded disabled:opacity-50"
+                hidden={messages.length != 1}
+                disabled={isLoading}
+                onClick={async () => {
+                  setIsLoading(true);
+                  const response = await fetch("api/tweets", {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      message: { role: "user", content: messages[0].content },
+                    }),
+                  });
+                  const data = await response.json();
+                  const cleanedJsonString = data.replace(/^```json\s*|```\s*$/g, '');
+                  const tweetsJson = JSON.parse(cleanedJsonString);
+                  tweetsJson.forEach((tweet: { tweet: string; }, index: number) => {
+                    const tweets: Message = {
+                      "id": "tweets",
+                      "role": "assistant",
+                      "content": tweet.tweet
+                    };
+                    messages.push(tweets);
+                  });
+                  setIsLoading(false);
+                }}>
+                Generate tweets
+              </button>
+              <button
+                className="w-full md:w-auto order-2 md:order-1 m-2 bg-green-700 hover:bg-gray-700 text-white py-2 px-4 rounded disabled:opacity-50"
                 hidden={messages.length < 2}
                 disabled={isLoading}
                 onClick={async () => {
@@ -153,7 +129,7 @@ export default function Chat() {
         {messages.length > 0 && !isLoading && (
           <div className="flex flex-col gap-2">
             <div className="space-y-2">
-              <h2 className="text-lg font-semibold tracking-tight">Here's the social network post:</h2>
+              <h2 className="text-lg text-green-500 font-semibold tracking-tight">Here's the social network post:</h2>
             </div>
             <textarea
               className="min-h-[100px] border text-black p-3 m-2 rounded"
@@ -162,30 +138,52 @@ export default function Chat() {
               value={messages[0].content}
               readOnly
             />
-            <a
-              className="text-center m-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
-              href="https://www.linkedin.com/sharing/share-offsite/" target="_blank"
-              onClick={() => copyText(messages[0].content)}>
-              Copy to clipboard & Open Linkedin
-            </a>
+            <div className="flex flex-row items-start text-center gap-2 m-2">
+              <p className="text-sm leading-6 b-0 text-white">
+                Click the logo to copy to clipboard & open in:
+              </p>
+              <a
+                className="hover:bg-green-700 text-white rounded disabled:opacity-50"
+                href="https://www.linkedin.com/sharing/share-offsite/" target="_blank"
+                onClick={() => copyText(messages[0].content)}>
+                <Image
+                  src="/images/linkedin-128.png" // Route of the image file
+                  height={30} // Desired size with correct aspect ratio
+                  width={30} // Desired size with correct aspect ratio
+                  alt="Send text to Linkedin"
+                />
+              </a>
+            </div>
+
           </div>
         )}
         {messages.length > 1 && !isLoading && (
           <div className="flex flex-col gap-2">
             <div className="space-y-2">
-              <h2 className="text-lg font-semibold tracking-tight">Here are the tweets:</h2>
+              <h2 className="text-lg text-green-500 font-semibold tracking-tight">Here are the tweets:</h2>
             </div>
             {messages.slice(1).map((tweet) => (
-              <div className="flex flex-col items-center">
+              <div className="flex flex-col items-center m-2">
                 <div
-                  className="w-full bg-white p-3 m-2 rounded border text-black"
+                  className="w-full bg-white p-3 rounded border text-black"
                 >{tweet.content}
                 </div>
-                <a className="text-center m-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50" 
-                href="https://twitter.com/intent/tweet?text=" target="_blank"
-                onClick={() => copyText(tweet.content)}>
-                  Copy to clipboard & Open Twitter
-                </a>
+                <div className="flex flex-row gap-2 m-2 align-middle">
+                  <span className="text-sm leading-6 text-white">
+                    Click the logo to copy and send the tweet to:
+                  </span>
+                  <a
+                    className="hover:bg-green-700 p-1 text-white rounded disabled:opacity-50"
+                    href="https://twitter.com/intent/tweet?text=" target="_blank"
+                    onClick={() => copyText(tweet.content)}>
+                    <Image
+                      src="/images/twitter-128.png" // Route of the image file
+                      height={20} // Desired size with correct aspect ratio
+                      width={20} // Desired size with correct aspect ratio
+                      alt="Send text to Twitter/X"
+                    />
+                  </a>
+                </div>
               </div>
             ))}
 
